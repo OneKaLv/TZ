89e651300bbd2979a2ef1b240ddba5930d363e7c
OneKaLv <60115765+OneKaLv@users.noreply.github.com>
2020-10-02 21:58:33 +0300

    Update Main.java

--- src/by/OneKa/Main.java
+++ src/by/OneKa/Main.java
@@ -8,7 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        new MySQL("localhost","antiscam","root","8pdSReCgpwqD");
+        new MySQL("","","","");
         new onBreak(this);
         new LastCommand().register();
     }
